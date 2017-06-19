c5e4a0a4b6c39a2ef9cf4c97571a7b783a5339a1
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-06-19 17:01:23 +0100

    Added NDIR class.

---
+++ scs_ndir/gas/ndir.py
@@ -0,0 +1,29 @@
+"""
+Created on 19 Jun 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+import time
+
+from scs_core.climate.sht_datum import SHTDatum
+
+from scs_host.bus.i2c import I2C
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+
+class NDIR(object):
+    """
+    Alphasense IRC-A1 NDIR CO2 Transmitter board
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        pass
+
